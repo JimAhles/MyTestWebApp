ec0a9d8a39e1be49d60330c231bc36f67669097c
JimAhles <cx1326ag@mnscu.edu>
2016-01-27 16:11:28 -0600

    Initial code creation.

---
+++ MyFixitTasks/dbo/Tables/FixItTasks.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[FixItTasks] (
+    [FixItTaskId] INT             IDENTITY (1, 1) NOT NULL,
+    [CreatedBy]   NVARCHAR (80)   NULL,
+    [Owner]       NVARCHAR (80)   NOT NULL,
+    [Title]       NVARCHAR (80)   NOT NULL,
+    [Notes]       NVARCHAR (1000) NULL,
+    [PhotoUrl]    NVARCHAR (200)  NULL,
+    [IsDone]      BIT             NOT NULL,
+    [DateCreated] DATETIME        DEFAULT ('1900-01-01T00:00:00.000') NOT NULL,
+    [email]       NVARCHAR (MAX)  NULL,
+    CONSTRAINT [PK_dbo.FixItTasks] PRIMARY KEY CLUSTERED ([FixItTaskId] ASC)
+);
+
---
+++ MyFixitTasks/dbo/Tables/FixItTasks_1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[FixItTasks] (
+    [FixItTaskId] INT             IDENTITY (1, 1) NOT NULL,
+    [CreatedBy]   NVARCHAR (80)   NULL,
+    [Owner]       NVARCHAR (80)   NOT NULL,
+    [Title]       NVARCHAR (80)   NOT NULL,
+    [Notes]       NVARCHAR (1000) NULL,
+    [PhotoUrl]    NVARCHAR (200)  NULL,
+    [IsDone]      BIT             NOT NULL,
+    CONSTRAINT [PK_dbo.FixItTasks] PRIMARY KEY CLUSTERED ([FixItTaskId] ASC)
+);
+
